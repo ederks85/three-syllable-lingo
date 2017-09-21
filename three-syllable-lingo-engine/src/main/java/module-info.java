77578a5e96b4d875153aea57d26458245536586f
Edwin Derks <edwin.derks@ordina.nl>
2017-09-21 20:36:47 +0200

    Update project to use Java 9

---
+++ three-syllable-lingo-engine/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module three.syllable.lingo.engine {
+    exports games.tsl.engine;
+    exports games.tsl.engine.api;
+    exports games.tsl.engine.api.exception;
+
+    requires transitive commons.lang3;
+    requires transitive commons.csv;
+}
---
+++ three-syllable-lingo-swing-client/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module three.syllable.lingo.swing.client {
+    requires three.syllable.lingo.engine;
+    requires java.desktop;
+}
